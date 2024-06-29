07435aac4e7f3ea7afc71e71647ee9530c4ad40e
Agung Fadhil <fadhilworkspace@gmail.com>
2024-06-29 11:55:31 +0700

    finish up render props

--- src/pages/Home/component.js
+++ src/pages/Home/component.js
@@ -1,6 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Flex, Divider, Row, Button } from "antd";
+import { Flex, Divider, Row, Button, ConfigProvider, Space } from "antd";
+import { TinyColor } from "@ctrl/tinycolor";
+
+const colors1 = ["#6253E1", "#04BEFE"];
+const colors2 = ["#fc6076", "#ff9a44", "#ef9d43", "#e75516"];
+const colors3 = ["#40e495", "#30dd8a", "#2bb673"];
+
+const getHoverColors = (colors) =>
+  colors.map((color) => new TinyColor(color).lighten(5).toString());
+const getActiveColors = (colors) =>
+  colors.map((color) => new TinyColor(color).darken(5).toString());
+
+function ButtonsAntdConfigured() {
+  return (
+    <Space>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: `linear-gradient(135deg, ${colors1.join(", ")})`,
+              colorPrimaryHover: `linear-gradient(135deg, ${getHoverColors(
+                colors1
+              ).join(", ")})`,
+              colorPrimaryActive: `linear-gradient(135deg, ${getActiveColors(
+                colors1
+              ).join(", ")})`,
+              lineWidth: 0,
+            },
+          },
+        }}
+      >
+        <Link to="render-props">
+          <Button type="primary" size="large">
+            Render Props
+          </Button>
+        </Link>
+      </ConfigProvider>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: `linear-gradient(90deg,  ${colors2.join(", ")})`,
+              colorPrimaryHover: `linear-gradient(90deg, ${getHoverColors(
+                colors2
+              ).join(", ")})`,
+              colorPrimaryActive: `linear-gradient(90deg, ${getActiveColors(
+                colors2
+              ).join(", ")})`,
+              lineWidth: 0,
+            },
+          },
+        }}
+      >
+        <Link to="/hoc">
+          <Button type="primary" size="large">
+            Higher Order Component (HOC)
+          </Button>
+        </Link>
+      </ConfigProvider>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: `linear-gradient(116deg,  ${colors3.join(", ")})`,
+              colorPrimaryHover: `linear-gradient(116deg, ${getHoverColors(
+                colors3
+              ).join(", ")})`,
+              colorPrimaryActive: `linear-gradient(116deg, ${getActiveColors(
+                colors3
+              ).join(", ")})`,
+              lineWidth: 0,
+            },
+          },
+        }}
+      >
+        <Link to="/compound-component">
+          <Button type="primary" size="large">
+            Compound Component
+          </Button>
+        </Link>
+      </ConfigProvider>
+    </Space>
+  );
+}
 
 export default function Home() {
   return (
@@ -9,41 +92,7 @@ export default function Home() {
         <h2>HOME</h2>
       </Divider>
       <Row>
-        <div style={{ display: "flex", gap: "10px" }}>
-          <Link to="/render-props">
-            <Button
-              style={{
-                background: "linear-gradient(45deg, #f3ec78, #af4261)",
-                borderRadius: "5px",
-                color: "#fff",
-              }}
-            >
-              <strong>Render Props</strong>
-            </Button>
-          </Link>
-          <Link to="/hoc">
-            <Button
-              style={{
-                background: "linear-gradient(45deg, #4facfe, #00f2fe)",
-                borderRadius: "5px",
-                color: "#fff",
-              }}
-            >
-              <strong>Higher Order Component (HOC)</strong>
-            </Button>
-          </Link>
-          <Link to="/compound-component">
-            <Button
-              style={{
-                background: "linear-gradient(45deg, #ff9a9e, #fad0c4)",
-                borderRadius: "5px",
-                color: "#fff",
-              }}
-            >
-              <strong>Compound Component</strong>
-            </Button>
-          </Link>
-        </div>
+        <ButtonsAntdConfigured />
       </Row>
     </Flex>
   );
--- src/pages/RenderProps/component.js
+++ src/pages/RenderProps/component.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { faker } from "@faker-js/faker";
 import {
   EditOutlined,
   EllipsisOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Avatar, Card, Flex, Divider, Row, Col } from "antd";
+import { Avatar, Card, Flex, Divider, Row, Col, QRCode, Space } from "antd";
 
 const { Meta } = Card;
+const GUTTER = 16;
+const SPAN_ITEM = 8;
 
-const dataRandomProfiles = Array.from({ length: 10 }, (_, i) => ({
-  avatar: faker.image.avatar(),
-  background: faker.image.urlPicsumPhotos(),
-  desc: `${faker.finance.amount({ min: 80 })}`,
-  name: `${faker.person.firstName()} ${faker.person.lastName()} | ${faker.internet.emoji()}`,
-}));
+const dataRandomProfiles = Array.from({ length: 10 }, (_, i) => {
+  const avatar = faker.image.avatarLegacy();
+  const bgUrl = faker.image.urlPicsumPhotos();
+  const desc = `${faker.finance.amount({ min: 80 })}`;
+  const emoji = faker.internet.emoji({ types: ["flag"] });
+  const name = `${faker.person.firstName()} ${faker.person.lastName()}, ${emoji}`;
+
+  return { avatar, bgUrl, desc, emoji, name };
+});
 
 const description = (desc) => (
-  <Flex justify="center" align="center">
+  <Flex align="center">
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg"
       style={{ width: 18, height: 18, marginRight: "5px" }}
@@ -27,32 +33,85 @@ const description = (desc) => (
   </Flex>
 );
 
+function ProfileCardItem({ item }) {
+  return (
+    <Col className="gutter-row" span={SPAN_ITEM}>
+      <Card
+        hoverable
+        cover={<img className="img-bg" alt="background" src={item.bgUrl} />}
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src={item.avatar} />}
+          title={item.name}
+          description={description(item.desc)}
+        />
+      </Card>
+    </Col>
+  );
+}
+
+function ProfileQRCodeItem({ item }) {
+  return (
+    <Col className="gutter-row col-center" span={SPAN_ITEM}>
+      <Space align="center">
+        <Card className="card-center">
+          <QRCode value={item.bgUrl} icon={item.avatar} />
+          <strong>{item.name}</strong>
+        </Card>
+      </Space>
+    </Col>
+  );
+}
+
+function List({ title, render }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const arrow = isOpen ? "⤵" : "⤴";
+  return (
+    <Col className="gutter-row" span={12}>
+      <Divider>
+        <h2 onClick={() => setIsOpen(!isOpen)}>
+          {title}&nbsp; {arrow}
+        </h2>
+      </Divider>
+      <Row gutter={[GUTTER, GUTTER]} wrap>
+        {isOpen && render}
+      </Row>
+    </Col>
+  );
+}
+
 export default function RenderProps() {
   return (
     <>
-      <Divider orientation="center">
-        <h2>Render Props</h2>
+      <Divider orientation="center" className="main-title">
+        <Link to="/">
+          <h2>Render Props</h2>
+        </Link>
       </Divider>
-      <Row gutter={[16, 16]} wrap>
-        {dataRandomProfiles &&
-          dataRandomProfiles.map((i, idx) => (
-            <Col key={idx} className="gutter-row" span={6}>
-              <Card
-                cover={<img alt="background" src={i.background} />}
-                actions={[
-                  <SettingOutlined key="setting" />,
-                  <EditOutlined key="edit" />,
-                  <EllipsisOutlined key="ellipsis" />,
-                ]}
-              >
-                <Meta
-                  avatar={<Avatar src={i.avatar} />}
-                  title={i.name}
-                  description={description(i.desc)}
-                />
-              </Card>
-            </Col>
-          ))}
+      <Row gutter={GUTTER} className="main-content">
+        <List
+          title="Profile Card"
+          render={
+            dataRandomProfiles &&
+            dataRandomProfiles.map((i, idx) => (
+              <ProfileCardItem key={idx} item={i} />
+            ))
+          }
+        />
+        <List
+          title="Profile QRCode"
+          render={
+            dataRandomProfiles &&
+            dataRandomProfiles.map((i, idx) => (
+              <ProfileQRCodeItem key={idx} item={i} />
+            ))
+          }
+        />
       </Row>
     </>
   );
